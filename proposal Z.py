cda95de02d40a3d8256a363289be0bdecd695b5e
JLR55555 <93500407+JLR55555@users.noreply.github.com>
2021-11-07 17:15:11 -0500

    Add files via upload

---
+++ proposal Z.py
@@ -0,0 +1,39 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+Horror Story Adventure
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+A website that lets you control every plots of a horror story and finally create your own speical story.
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+We will create this website by using HTML, CSS, JAVASCRIPT, and Python. HTML, CSS, and JAVASCRIPT will help us design the website's style, and we use python to build the whole game.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+No.
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+1. Hirab Abdourazak, leutix.111@gmail.com, TA:Zhenyu Zhao; 2. Deborah Oguche, tul48016@temple.edu, TA: Sai Shi.
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+I want to finish the whole game before or at the deadline.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+I want to finish the whole game before or at the deadline.
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+I want to finish the whole game before or at the deadline.
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+Our next steps is finding a good story we like as a foundation. We need to know how to use HTML, CSS, and JAVASCRIPT. The topics we need to research are horror stories, and horror style web-design.
